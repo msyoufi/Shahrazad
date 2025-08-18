5d1128bd2510808545fcd45c9a406726e4d013db
msyoufi <msyoufi.dev@gmail.com>
2025-08-18 16:53:48 +0200

    WIP: setup the dashboard component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home'
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: Home }
+  { path: '', component: Home },
+  {
+    path: 'admin-1975-dashboard',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/dashboard/dashboard')
+      .then(comp => comp.Dashboard)
+  }
 ];
