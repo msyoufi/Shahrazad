db672bd9d8dc7a58179b45700c4bc549f9c767c7
msyoufi <msyoufi.dev@gmail.com>
2025-08-19 18:13:49 +0200

    fix: fix the click event handling and ad ng-content

--- src/app/shared/components/button/shari-button.ts
+++ src/app/shared/components/button/shari-button.ts
@@ -7,25 +7,26 @@ import { Component, input, output } from '@angular/core';
     <button 
       [type]="type()" 
       [class]="'shari-button ' + variant() + ' ' + color() + ' ' + class()"
-      (click)="onClick()"
+      [class.loading]='loading()'
+      (click)="handleClick()"
       [disabled]="disabled()"
       >
-        {{text()}}
+      <ng-content />
     </button>
     `,
   styleUrl: './shari-button.scss'
 })
 export class ShariButton {
-  text = input.required<string>();
   variant = input<'filled' | 'outlined'>('filled');
   color = input<'primary' | 'secondary' | 'red' | 'green'>('primary');
   type = input<'button' | 'submit' | 'reset'>('button');
   class = input<string>('');
   disabled = input<boolean>(false);
+  loading = input<boolean>(false);
 
-  click = output<void>();
+  onClick = output<void>();
 
-  onClick(): void {
-    this.click.emit();
+  handleClick(): void {
+    this.onClick.emit();
   }
 }
