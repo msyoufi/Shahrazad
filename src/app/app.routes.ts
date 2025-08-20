38bf7c47c25068b3be35cbbe574f04a9b04c61bb
msyoufi <msyoufi.dev@gmail.com>
2025-08-20 13:01:44 +0200

    WIP: create the admin login interface

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,13 @@ import { PaintingsOverview } from './pages/dashboard/paintings-overview/painting
 export const routes: Routes = [
   { path: '', component: Home },
   {
+    path: 'admin-1975',
+    loadComponent: () => import('./pages/admin-login/admin-login').then(comp => comp.AdminLogin)
+  },
+  {
     path: 'admin-1975-dashboard',
     canActivate: [authGuard],
-    loadComponent: () => import('./pages/dashboard/dashboard')
-      .then(comp => comp.Dashboard),
+    loadComponent: () => import('./pages/dashboard/dashboard').then(comp => comp.Dashboard),
     children: [
       { path: 'overview', component: PaintingsOverview },
     ]
---
+++ src/app/pages/admin-login/admin-login.ts
@@ -0,0 +1,81 @@
+import { Component, inject, signal, viewChild } from '@angular/core';
+import { AuthService } from '../../shared/services/auth';
+import { Router } from '@angular/router';
+import { Snackbar } from '../../shared/components/snackbar';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ShariButton } from '../../shared/components/button/shari-button';
+import { PasswordToggle } from '../../shared/components/password-toggle';
+import { FirebaseError } from '@angular/fire/app';
+
+@Component({
+  selector: 'shari-admin-login',
+  imports: [FormsModule, MatProgressSpinner, ShariButton, PasswordToggle],
+  templateUrl: './admin-login.html',
+  styleUrl: './admin-login.scss'
+})
+export class AdminLogin {
+  authService = inject(AuthService);
+  router = inject(Router);
+  snackbar = inject(Snackbar);
+
+  form = viewChild.required<NgForm>('form');
+  isLoggingIn = signal(false);
+  isResetingPW = signal(false);
+
+
+  async onSubmit(): Promise<void> {
+    if (this.form().controls['email']?.invalid) {
+      this.snackbar.show('Invalid Email Address!', 'red');
+      return;
+    }
+
+    if (this.form().controls['password']?.invalid) {
+      this.snackbar.show('Invalid Password!', 'red');
+      return;
+    }
+
+    this.isLoggingIn.set(true);
+    const { email, password } = this.form().value;
+
+    try {
+      await this.authService.login(email, password);
+      await this.router.navigateByUrl('/admin-1975-dashboard/overview');
+      this.snackbar.show('Logged In');
+
+    } catch (err: unknown) {
+      let message = 'Login Failed. Unknown Error!';
+
+      if (err instanceof FirebaseError) {
+        if (err.code === 'auth/invalid-credential')
+          message = 'Email Or Password Incorrect';
+      }
+
+      this.snackbar.show(message, 'red', 4000);
+
+    } finally {
+      this.isLoggingIn.set(false);
+    }
+  }
+
+  async onResetPasswordClick(): Promise<void> {
+    if (this.form().controls['email']?.invalid) {
+      this.snackbar.show('Invalid Email Address!', 'red');
+      return;
+    }
+
+    this.isResetingPW.set(true);
+    const email = this.form().value.email;
+
+    try {
+      await this.authService.resetPassword(email);
+      this.snackbar.show(`Reset Link Sent To:\n ${email}`, 'green', 5000);
+
+    } catch (err: unknown) {
+      this.snackbar.show(`${err}`, 'red', 4000);
+
+    } finally {
+      this.isResetingPW.set(false);
+    }
+  }
+}
