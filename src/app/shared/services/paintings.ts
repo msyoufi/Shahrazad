161e57ab9f191ec58852937a20c93b21d8be7123
msyoufi <msyoufi.dev@gmail.com>
2025-08-22 10:11:33 +0200

    WIP: implement the paintings reordering logic

--- src/app/shared/services/paintings.ts
+++ src/app/shared/services/paintings.ts
@@ -44,7 +44,28 @@ export class PaintingsService implements OnDestroy {
     return updateDoc(this.getDocRef(id), paintingData);
   }
 
-  updatePaintingsOrder(paintings: Painting[]): Promise<void> {
+  deletePainting(id: string): Promise<void> {
+    return deleteDoc(this.getDocRef(id));
+  }
+
+  updatePaintingsOrder(paintingId: string, newOrder: number, isDelete: boolean = false): Promise<void> {
+    const list = this.paintings.slice();
+
+    const index = list.findIndex(p => p.id === paintingId);
+    if (index < 0) throw new Error();
+
+    const [moved] = list.splice(index, 1);
+
+    if (!isDelete) {
+      const newIndex = Math.max(0, Math.min(newOrder - 1, list.length));
+      list.splice(newIndex, 0, moved);
+    }
+
+    return this.writeNewOrder(list);
+  }
+
+  private writeNewOrder(paintings: Painting[]): Promise<void> {
+    console.log('updating order')
     const batch = writeBatch(this.db);
 
     paintings.forEach((p, i) =>
@@ -52,10 +73,6 @@ export class PaintingsService implements OnDestroy {
     );
 
     return batch.commit();
-  }
-
-  deletePainting(id: string): Promise<void> {
-    return deleteDoc(this.getDocRef(id));
   }
 
   private getDocRef(id: string): DocumentReference {
