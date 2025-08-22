afd239b529a4a59427a11f495ad2890ad4610e62
msyoufi <msyoufi.dev@gmail.com>
2025-08-22 12:07:50 +0200

    WIP: improve the header's design

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth';
+import { Snackbar } from '../snackbar';
+import { ShariButton } from '../button/shari-button';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'shari-header',
-  imports: [],
+  imports: [RouterLink, ShariButton, MatProgressSpinner],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  authService = inject(AuthService);
+  router = inject(Router);
+  snackbar = inject(Snackbar);
 
+  isLoggingOut = signal(false);
+
+
+  async onLogoutClick(): Promise<void> {
+    this.isLoggingOut.set(true);
+
+    try {
+      await this.authService.logout();
+      await this.router.navigateByUrl('');
+
+      this.snackbar.show('Logged Out');
+
+    } catch (err: unknown) {
+      this.snackbar.show(`${err}`, 'red', 4000);
+
+    } finally {
+      this.isLoggingOut.set(false);
+    }
+  }
 }
