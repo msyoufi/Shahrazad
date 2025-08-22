fff01e0b746c991ff00f5ea6f237d35232c9e3dd
msyoufi <msyoufi.dev@gmail.com>
2025-08-22 12:08:14 +0200

    WIP: remove the logout link from the dashborad navbar

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,24 +1,18 @@
 import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
-import { AuthService } from '../../shared/services/auth';
-import { Snackbar } from '../../shared/components/snackbar';
-import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'shari-dashboard',
-  imports: [RouterOutlet, RouterLink, MatProgressSpinner],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
 export class Dashboard {
-  authService = inject(AuthService);
   router = inject(Router);
-  snackbar = inject(Snackbar);
   destroyRef = inject(DestroyRef);
 
   currentTab = signal('');
-  isLoggingOut = signal(false);
 
   constructor() {
     this.listenToPathChnages();
@@ -33,22 +27,5 @@ export class Dashboard {
           this.currentTab.set(path);
         }
       });
-  }
-
-  async onLogoutClick(): Promise<void> {
-    this.isLoggingOut.set(true);
-
-    try {
-      await this.authService.logout();
-      await this.router.navigateByUrl('');
-
-      this.snackbar.show('Logged Out');
-
-    } catch (err: unknown) {
-      this.snackbar.show(`${err}`, 'red', 4000);
-
-    } finally {
-      this.isLoggingOut.set(false);
-    }
   }
 }
