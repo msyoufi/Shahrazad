3eb3d5030a1ce9d8849f855260185bab39d779b3
msyoufi <msyoufi.dev@gmail.com>
2026-03-11 22:47:20 +0100

    redesign the header and the footer

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,13 +1,10 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { AuthService } from '../../services/auth';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'shari-footer',
-  imports: [RouterLink],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrl: './footer.scss',
 })
 export class Footer {
-  authService = inject(AuthService);
+  currentYear = new Date().getFullYear();
 }
--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,38 +1,14 @@
-import { Component, inject, signal } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth';
-import { Snackbar } from '../snackbar';
-import { ShariButton } from '../button/shari-button';
-import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'shari-header',
-  imports: [RouterLink, ShariButton, MatProgressSpinner],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
 export class Header {
   authService = inject(AuthService);
   router = inject(Router);
-  snackbar = inject(Snackbar);
-
-  isLoggingOut = signal(false);
-
-
-  async onLogoutClick(): Promise<void> {
-    this.isLoggingOut.set(true);
-
-    try {
-      await this.authService.logout();
-      await this.router.navigateByUrl('');
-
-      this.snackbar.show('Logged Out');
-
-    } catch (err: unknown) {
-      this.snackbar.show(`${err}`, 'red', 4000);
-
-    } finally {
-      this.isLoggingOut.set(false);
-    }
-  }
 }
