e9fcf37c0c0d0453d2965f52499ccf81fcd7e90d
msyoufi <msyoufi.dev@gmail.com>
2026-04-19 10:51:58 +0200

    remove the animation of the story blocks

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,15 +1,6 @@
 import { ProfileService } from '../../shared/services/profile';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
-import {
-  Component,
-  inject,
-  signal,
-  AfterViewInit,
-  OnDestroy,
-  ViewChildren,
-  QueryList,
-  ElementRef,
-} from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'shari-about',
@@ -17,41 +8,9 @@ import {
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
-export class About implements AfterViewInit, OnDestroy {
+export class About {
   profileService = inject(ProfileService);
-
-  @ViewChildren('animateOnScroll') animateElements!: QueryList<ElementRef>;
-  private observer?: IntersectionObserver;
 
   isLoadingCoverImg = signal(true);
   isLoadingProfileImg = signal(true);
-
-  ngAfterViewInit() {
-    this.observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-            this.observer?.unobserve(entry.target);
-          }
-        });
-      },
-      { threshold: 0.15 },
-    );
-
-    this.observeElements();
-    this.animateElements.changes.subscribe(() => this.observeElements());
-  }
-
-  private observeElements() {
-    this.animateElements.forEach((el) => {
-      if (!el.nativeElement.classList.contains('visible')) {
-        this.observer?.observe(el.nativeElement);
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    this.observer?.disconnect();
-  }
 }
